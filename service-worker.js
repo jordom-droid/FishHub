afd124f55b51d74f9abd41485ffa71436c3377a5
jordom-droid <jordom@gmail.com>
2026-09-22 16:56:40 +1000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,1 @@
-// FishWindow v6 — unregister legacy caches.
-self.addEventListener("install",()=>self.skipWaiting());
-self.addEventListener("activate",event=>event.waitUntil(Promise.all([self.registration.unregister(),self.clients.claim()])));
+self.addEventListener('install',()=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.registration.unregister(),self.clients.claim()])));
